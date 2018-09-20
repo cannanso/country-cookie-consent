5ba269f225f3ae198d5dc04ea8d397b14dc19b1b
Claire Annan <claire.annan.me@gmail.com>
2018-09-20 09:27:39 -0400

    Update plugin license and version info.

--- country-cookie-consent.php
+++ country-cookie-consent.php
@@ -7,13 +7,14 @@
  * Plugin URI: http://www.cannanso.com
  * Developer: Claire Annan of cannanso
  * Developer URI: http://www.cannanso.com
- * Version: 0.1.1
+ * Version: 0.1.2
+ * License: GPL 3.0
  *
  * Copyright 2018 cannanso
  */
 
 define( 'PLUGIN_DIR', dirname( __FILE__ ) );
-define( 'PLUGIN_VER', '0.1.1' );
+define( 'PLUGIN_VER', '0.1.2' );
 define( 'PLUGIN_URL', plugins_url( '', __FILE__ ) );
 
 
